a45b193c895125a831d796b3943ed70dc0eac3bb
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-05-25 18:44:12 +0530

    routes and setup add

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,10 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError } from "@ethan-hunt/common";
+import { errorHandler, NotFoundError, currentUser } from "@ethan-hunt/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
