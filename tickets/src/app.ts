c2a4d3d4376c241334b70c1dac31e6ca5dfb92cb
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-05-26 07:48:06 +0530

    ticket update add

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from "@ethan-hunt/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { indexTicketRouter } from "./routes/index";
+import { updatetTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -23,6 +24,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updatetTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -41,7 +41,7 @@ afterAll(async () => {
 
 global.signin = () => {
   const payload = {
-    id: "8fnbw1",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
